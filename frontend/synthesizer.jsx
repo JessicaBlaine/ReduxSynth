a9ad46f6869f42eac2acf1a1157e7c5312ff72f8
Work <joshuablaine@users.noreply.github.com>
2016-09-28 17:49:27 -0700

    added note and tones untilities

--- frontend/synthesizer.jsx
+++ frontend/synthesizer.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Note from './util/note';
+
+window.Note = Note;
+
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
   ReactDOM.render(<div />, rootEl);

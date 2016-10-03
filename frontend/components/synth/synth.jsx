cbef569c3b0521385fe083b624a2acbdab359f7f
Work <joshuablaine@users.noreply.github.com>
2016-10-02 17:06:33 -0700

    basic Synth component functionality

--- frontend/components/synth/synth.jsx
+++ frontend/components/synth/synth.jsx
@@ -1,7 +1,22 @@
 import React, { PropTypes } from 'react';
+import { NOTE_NAMES, TONES } from '../../util/tones';
+import Note from '../../util/note';
 
-const Synth = () => (
-  <div>Synth</div>
-);
+class Synth extends React.Component {
+  constructor(props) {
+    super(props);
+    this.notes = NOTE_NAMES.map(n => new Note(TONES[n]));
+  }
 
-export default Synth;
+  render() {
+    return (
+      <ul>
+        {
+          this.notes.map(n => <li>{n.freq}</li>)
+        }
+      </ul>)
+    ;
+  }
+}
+
+export default Synth ;

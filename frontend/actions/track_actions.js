7f3d23bd21c26bfdd85eac2d2c143643fafeebef
Work <joshuablaine@users.noreply.github.com>
2016-11-02 16:09:42 -0700

    track actions

--- frontend/actions/track_actions.js
+++ frontend/actions/track_actions.js
@@ -4,4 +4,16 @@ export const ADD_NOTES = 'ADD_NOTES';
 
 export const startRecording = () => ({
   type: START_RECORDING,
+  timeNow: Date.now(),
+});
+
+export const stopRecording = () => ({
+  type: STOP_RECORDING,
+  timeNow: Date.now(),
+});
+
+export const addNotes = notes => ({
+  type: ADD_NOTES,
+  timeNow: Date.now(),
+  notes,
 });
